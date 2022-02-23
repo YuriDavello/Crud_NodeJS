5c1666c6efcd189f88c0c990a7703b28e16ce303
yuridavello <yuridavello@hotmail.com>
2022-02-23 10:26:15 -0300

    estrutura da aplicação

---
+++ app/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET', 'POST', 'PUT', 'DELETE');
+  next();
+});
